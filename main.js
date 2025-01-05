aa4201ed663a18c5a63ed37fc7794acb36c2c8b6
Gabriel Muniz <56240854+Gabriel-Muniz@users.noreply.github.com>
2025-01-05 14:13:14 -0300

    :sparkles:FEAT: Cria deck com 52 cartas

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-console.log('Teste')
+const valorCartas = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+const naipeCartas = ["♦️", "♠️", "♥️", "♣️"];
+
+const deckCartas = [];
+
+function Carta(valor, naipe) {
+  this.valor = valor;
+  this.naipe = naipe;
+}
+
+function criarCartas() {
+  valorCartas.map((valor) => {
+    naipeCartas.forEach((naipe) => {
+      const novaCarta = new Carta(valor, naipe);
+      deckCartas.push(novaCarta);
+    })
+  })
+}
+
+criarCartas();
+console.log(deckCartas);
