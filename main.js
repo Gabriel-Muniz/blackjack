6f9d8c66c0213e86be9f62dfd21443a438687ae6
Gabriel Muniz <56240854+Gabriel-Muniz@users.noreply.github.com>
2025-01-05 15:27:10 -0300

    :sparkles:FEAT: Checar disponibilidade da carta

--- main.js
+++ main.js
@@ -2,20 +2,39 @@ const valorCartas = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
 const naipeCartas = ["♦️", "♠️", "♥️", "♣️"];
 
 const deckCartas = [];
+const cartasIndisponiveis = [];
 
 function Carta(valor, naipe) {
   this.valor = valor;
   this.naipe = naipe;
 }
 
-function criarCartas() {
+const criarCartas = (function () {
   valorCartas.map((valor) => {
     naipeCartas.forEach((naipe) => {
       const novaCarta = new Carta(valor, naipe);
       deckCartas.push(novaCarta);
-    })
-  })
+    });
+  });
+})();
+
+function obterCartaAleatoria() {
+  let aux;
+  do {
+    aux = Math.floor(Math.random() * 52);
+  } while (!checarCartaValida(deckCartas[aux]));
+  
+  cartasIndisponiveis.push(deckCartas[aux]);
+  return aux;
 }
 
-criarCartas();
-console.log(deckCartas);
+function checarCartaValida(carta) {
+  const cartaSorteada = cartasIndisponiveis.find(
+    (cartaIndisponivel) => cartaIndisponivel === carta
+  );
+
+  if (cartaSorteada) {
+    return false;
+  }
+  return true;
+}
